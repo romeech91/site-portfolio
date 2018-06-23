1ec8bc420a3bcd71450182e203c916e2c4a0afb3
Roman Kazakov <romontino@gmail.com>
2018-06-23 18:57:58 +0300

    slider

--- src/assets/scripts/modules/skills.js
+++ src/assets/scripts/modules/skills.js
@@ -1,9 +1,34 @@
 import Vue from "vue";
 
+const skill = {
+    template: "#skill",
+    props: {
+        skillName: String,
+        skillPercents: Number
+    },
+    methods: {
+        drawCircle() {
+            const circle = this.$refs["color-circle"];
+            const dashOffset = parseInt(
+                getComputedStyle(circle).getPropertyValue("stroke-dashoffset")
+            ); 
+            const percents = (dashOffset / 100) * (100 - this.skillPercents);
+            circle.style.strokeDashoffset = percents;
+        }
+    },
+    mounted() {
+        this.drawCircle();
+    }
+};
+
+
 const skillsRow = {
     template: "#skills-row",
+    components: {
+        skill
+    },
     props: {
-        skillGroupName: String
+        skill: Object
     }
 };
 
