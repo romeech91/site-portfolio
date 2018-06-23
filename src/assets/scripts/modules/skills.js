887cc6dfee0e8f0639def11f6450f00bf71c0d7d
Roman Kazakov <romontino@gmail.com>
2018-06-24 00:04:40 +0300

    slider/footer

--- src/assets/scripts/modules/skills.js
+++ src/assets/scripts/modules/skills.js
@@ -1,49 +1,48 @@
 import Vue from "vue";
 
 const skill = {
-    template: "#skill",
-    props: {
-        skillName: String,
-        skillPercents: Number
-    },
-    methods: {
-        drawCircle() {
-            const circle = this.$refs["color-circle"];
-            const dashOffset = parseInt(
-                getComputedStyle(circle).getPropertyValue("stroke-dashoffset")
-            ); 
-            const percents = (dashOffset / 100) * (100 - this.skillPercents);
-            circle.style.strokeDashoffset = percents;
-        }
-    },
-    mounted() {
-        this.drawCircle();
+  template: "#skill",
+  props: {
+    skillName: String,
+    skillPercents: Number
+  },
+  methods: {
+    drawCircle() {
+      const circle = this.$refs["color-circle"];
+      const dashOffset = parseInt(
+        getComputedStyle(circle).getPropertyValue("stroke-dashoffset")
+      );
+      const percents = (dashOffset / 100) * (100 - this.skillPercents);
+      circle.style.strokeDashoffset = percents;
     }
+  },
+  mounted() {
+    this.drawCircle();
+  }
 };
 
-
 const skillsRow = {
-    template: "#skills-row",
-    components: {
-        skill
-    },
-    props: {
-        skill: Object
-    }
+  template: "#skills-row",
+  components: {
+    skill
+  },
+  props: {
+    skill: Object
+  }
 };
 
 new Vue({
-    el: "#skills-container",
-    components: {
-        skillsRow
-    },
-    data: {
-        skills: {}
-    },
-    created() {
-        const data = require("../../../data/skills.json");
-        this.skills = data;
-        console.log(data);
-    },
-    template: "#skills-list"
+  el: "#skills-container",
+  components: {
+    skillsRow
+  },
+  data: {
+    skills: {}
+  },
+  created() {
+    const data = require("../../../data/skills.json");
+    this.skills = data;
+    console.log(data);
+  },
+  template: "#skills-list"
 });
--- src/assets/scripts/modules/slider.js
+++ src/assets/scripts/modules/slider.js
@@ -1,6 +1,21 @@
 import Vue from "vue";
 
+const info = {
+  template: "#slider-info"
+};
+
+const display = {
+  template: "#slider-display"
+};
+
+const buttons = {
+  template: "#slider-buttons"
+};
+
 new Vue({
-    el: "#slider-component",
-    template: "#slider"
+  el: "#slider-component",
+  components: {
+    info, display, buttons
+  },
+  template: "#slider"
 });
