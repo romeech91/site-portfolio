c5999cb4467fa9bfc04e281c986276911552c2d3
Roman Kazakov <romontino@gmail.com>
2018-06-25 23:39:35 +0300

    flip-card

---
+++ src/assets/scripts/auth.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector('.auth__btn');
+const frontSide = document.querySelector('.hero__bg-block');
+const backSide = document.querySelector('.hero__bg-block_auth');
+const goMain = document.querySelector('.link__button-back');
+
+btn.addEventListener("click", () => {
+    frontSide.style.transform = 'rotateY(180deg)';
+    backSide.style.transform = 'rotateY(360deg)';
+});
+
+goMain.addEventListener('click', (e) => {
+    e.preventDefault();
+    frontSide.style.transform = 'rotateY(0deg)';
+    backSide.style.transform = 'rotateY(180deg)';
+})
--- src/assets/scripts/modules/ham-menu.js
+++ src/assets/scripts/modules/ham-menu.js
@@ -1,35 +1,7 @@
-$(function () {
-  let isOn = false;
-  const hamToggle = $('#ham-toggle');
-  const hamburgerMenu = $('#hamburger-menu');
-  const hamburgerMenuBg = $('#hamburger-menu-bg');
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".hero__nav");
 
-  const hide = () => {
-    hamToggle.removeClass("on");
-    hamburgerMenu.removeClass("on");
-    hamburgerMenuBg.removeClass("on");
-    hamburgerMenuBg.addClass("off");
-    isOn = false;
-  };
-
-  $('body').on('click', function (e) {
-    if ($(e.target).closest('#ham-toggle').length) {
-      if (!isOn) {
-        hamToggle.addClass("on");
-        hamburgerMenu.addClass("on");
-        hamburgerMenuBg.addClass("on");
-        hamburgerMenuBg.removeClass("off");
-        isOn = true;
-      } else {
-        if (isOn) {
-          hide();
-        }
-      }
-    } else if (!$(e.target).closest('.hamburger-menu__list').length) {
-      if (isOn) {
-        hide();
-      }
-    }
-
-  });
-})
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active");
+  nav.classList.toggle("hero__nav--active");
+});
