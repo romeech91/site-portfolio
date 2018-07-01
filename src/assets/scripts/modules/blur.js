30d24e69865d9a7cce30327c47e207adf72aedc4
Roman Kazakov <romontino@gmail.com>
2018-07-01 21:41:34 +0300

    blur fix

--- src/assets/scripts/modules/blur.js
+++ src/assets/scripts/modules/blur.js
@@ -1,16 +1,26 @@
 var blur = (function () {
     var wrapper = document.querySelector('.form'),
         form = document.querySelector('.form__blur');
+    
+    function getCoords(elem) {
+        const box = elem.getBoundingClientRect();
 
+        return {
+            top: box.top + window.pageYOffset,
+            left: box.left + window.pageXOffset
+        };
+    }
     return {
         set: function () {
+            let top = getCoords(wrapper).top - getCoords(wrapper.parentNode).top;
+
             var imgWidth = document.querySelector('.about-me').offsetWidth,
                 posLeft = -wrapper.offsetLeft,
                 posTop = -wrapper.offsetTop,
                 blurCSS = form.style;
                 
             blurCSS.backgroundSize = imgWidth + 'px' + ' ' + 'auto';
-            blurCSS.backgroundPosition = posLeft + 'px' + ' ' + posTop + 'px';
+            blurCSS.backgroundPosition = posLeft + 'px' + ' ' + -top + 'px';
         }
     }
 }());
