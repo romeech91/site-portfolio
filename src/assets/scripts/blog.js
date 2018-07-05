54280eab887c174e7169a64e51cf531e7969bec0
Roman Kazakov <romontino@gmail.com>
2018-07-05 12:28:22 +0300

    mob-sidebar bug fixed

--- src/assets/scripts/blog.js
+++ src/assets/scripts/blog.js
@@ -2,4 +2,4 @@ import "./modules/preloader.js";
 import "./modules/ham-menu.js";
 import "./modules/sidebar-blog.js"
 import "./modules/blog-navigate.js"
-import "./modules/blog-nav-sidebar.js"
+import "./modules/blog-nav-mobile.js"
