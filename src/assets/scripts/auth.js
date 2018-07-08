851898855c95c90e50fd1c267313771f0df2ef81
Roman Kazakov <romontino@gmail.com>
2018-07-08 21:34:39 +0300

    admin_beginning

--- src/assets/scripts/auth.js
+++ src/assets/scripts/auth.js
@@ -1,4 +1,4 @@
 import "./modules/preload.js";
 import "./modules/parallax-move.js";
 import "./modules/flip-card-move.js";
-import "./modules/login.js";
+import "./modules/login.js"
--- src/assets/scripts/modules/login.js
+++ src/assets/scripts/modules/login.js
@@ -1,6 +1,25 @@
-
 import Vue from "vue";
+import axios from 'axios';
 
 new Vue({
-    el: "#auth-component"
+    el: "#auth-component",
+    data() {
+        return {
+            user: {
+                name: "",
+                password: ""
+            }
+        };
+    },
+    methods: {
+        login() {
+            axios.post('http://localhost:8000/login', this.user).then(response => {
+                if(response.status === 200) {
+                    const ttl = Math.floor(Date.now() / 1000 + response.data.ttl);
+                    localStorage.setItem('token', response.data.token);
+                    localStorage.setItem("ttl", ttl);
+                }
+            });
+        }
+    }
 });
