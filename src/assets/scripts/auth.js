620df60aa405fcfccb4582fd34e01b08b4adddf7
Roman Kazakov <romontino@gmail.com>
2018-07-08 18:50:59 +0300

    admin-begin

--- src/assets/scripts/auth.js
+++ src/assets/scripts/auth.js
@@ -1,4 +1,4 @@
-import "./modules/parallax-move.js";
-import "./modules/flip-card-move.js"
 import "./modules/preload.js";
-
+import "./modules/parallax-move.js";
+import "./modules/flip-card-move.js";
+import "./modules/login.js";
